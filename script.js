82874236a11d3a170f32d2a075c00c6d7eba8a20
nikit-p <85838488+nikit-p@users.noreply.github.com>
2024-08-20 18:39:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+/* Get stopwatch and button ID */
+const stopwatchDuration = document.getElementById("stopwatchDuration"),
+    start = document.getElementById("start"),
+    lap = document.getElementById("lap"),
+    stop = document.getElementById("stop"),
+    reset = document.getElementById("reset"),
+    laps = document.getElementById("laps");
+
+/* Global declaration */
+let hrs = 0,
+    mins = 0,
+    sec = 0,
+    ms = 0,
+    timeInterval;
+
+/* start button event and start timer */
+start.onclick = () => {
+    timeInterval = setInterval(() => {
+        ms++;
+
+        if (ms == 100) {
+            sec++;
+            ms = 0;
+        }
+        if (sec == 60) {
+            mins++;
+            sec = 0;
+        }
+        if (mins == 60) {
+            hrs++;
+            mins = 0;
+        }
+        stopwatchDuration.innerHTML = `${zeroPad(hrs)}:${zeroPad(mins)}:${zeroPad(sec)}:${zeroPad(ms)}`;
+    }, 10);
+
+    start.setAttribute("style", "display:none");
+    stop.setAttribute("style", "display:block");
+    lap.setAttribute("style", "display:block");
+    reset.setAttribute("style", "display:none");
+    lap.removeAttribute("disabled");
+};
+
+/* Add zero's to timer */
+const zeroPad = (num) => {
+    return String(num).padStart(2, "0");
+};
+
+/* Add laps in stopwatch */
+let count = 0;
+lap.onclick = () => {
+    count++;
+    let li = document.createElement("li");
+    li.innerHTML = `${"#" + count} - ${zeroPad(hrs)}:${zeroPad(mins)}:${zeroPad(sec)}:${zeroPad(ms)}`;
+    laps.appendChild(li);
+    laps.scroll({ top: laps.scrollHeight, behavior: "smooth" });
+};
+
+/* stop the timer and change the buttons */
+stop.onclick = () => {
+    clearInterval(timeInterval);
+
+    lap.setAttribute("style", "display:none");
+    reset.setAttribute("style", "display:block");
+    start.setAttribute("style", "display:block");
+    start.innerHTML = "Resume";
+    stop.setAttribute("style", "display:none");
+};
+
+/* reset the timer */
+reset.onclick = () => {
+    laps.innerHTML = "";
+    hrs = mins = sec = ms = count = 0;
+    clearInterval(timeInterval);
+    stopwatchDuration.innerHTML = "00:00:00:00";
+
+    start.innerHTML = "Start";
+    lap.setAttribute("style", "display:block");
+    lap.setAttribute("disabled", true);
+    reset.setAttribute("style", "display:none");
+    start.setAttribute("style", "display:block");
+    stop.setAttribute("style", "display:none");
+};
